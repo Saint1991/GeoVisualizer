b8aebaddb929093584abf81e1518d886de9705b0
Saint1991 <se-mizuno@hotmail.co.jp>
2014-12-10 22:07:01 +0900

    Second Commit

--- js/controllers/app-controller.js
+++ js/controllers/app-controller.js
@@ -1,7 +1,9 @@
 'use strict';
+
 (function() {
 
 	var appModule = angular.module('geovisualizer');
-	var filelistModule = angular.module('geovisualizer.filelist');
-	var mapModule = angular.module('geovisualizer.map');
+	appModule.controller('appController', ['$scope', function($scope) {
+		$scope.datas = [];
+	}]);
 })();
--- js/controllers/filelist-controller.js
+++ js/controllers/filelist-controller.js
@@ -10,20 +10,56 @@
 		return;
 	}
 
+
+	//Definitions of Directive
+	fileListModule.directive('fileChange', ['$parse', function($parse) {
+
+		var fileChangeDef = {
+			
+			require: 'ngModel',
+
+			restrict: 'A',
+			
+			scope: false,
+
+			template: '',
+			
+			link: function ($scope, element, attrs, ngModel) {
+
+				var fileChange = attrs['fileChange'];
+;				var attrHandler = $parse(attrs['fileChange']);
+				var handler = function(e) {
+					$scope.$apply(function() {
+						attrHandler($scope, {$event: e, files: e.target.files});
+					});
+				};
+
+				var el = element[0];
+				$(element[0]).on('change', handler);
+
+			}
+		};
+
+		return fileChangeDef;
+	}]);
+
+	
+	//Definitions of Controller
 	fileListModule.controller('filelistController', ['$scope', function($scope) {
 		
 		//this is  a filel list loaded so far
-		$scope.fileList = ['test'];
+		$scope.fileList = [];
 
 		//this is an array that receive the change of input type="file"
 		$scope.selectedFiles = [];
 
 		//this event listener is called when files are loaded
-		$scope.filelistChanged = function($event) {
-			console.log($event);
+		$scope.filelistChanged = function($event, files) {
+			for (var i = 0; i < files.length; i++) {
+				$scope.$parent.datas.push(files);
+			}
 		};
 
-		$scope.test="loaded";
 	}]);
 
 
